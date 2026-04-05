7ce11411f565231e34d071af28856227f6e0c419
Teo <mmanchola12temporal@gmail.com>
2026-04-05 02:42:41 +0200

    feat(app): integrate firebase provider and bootstrap new UI theme

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyB2TySez00zpXhC7_Yskfk4YS4frR-jjn4',
+    authDomain: 'tutto-a-lena-cms.firebaseapp.com',
+    projectId: 'tutto-a-lena-cms',
+    storageBucket: 'tutto-a-lena-cms.firebasestorage.app',
+    messagingSenderId: '813461022908',
+    appId: '1:813461022908:web:6f45a0327002e7344448b9',
+  },
+}
